65a801cbecdc28858a217822ae677ddfde205e27
step22 <vukasin123@gmail.com>
2017-02-28 16:08:11 +0100

    sajt turisticke agencije

---
+++ Stepina Agency/js/script.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function(){
+
+	// Mobile menu
+	$('.mobile-menu-icon').click(function(){
+		$('.vnav').toggleClass('show');
+	});
+  
+  	$('body').bind('touchstart', function() {});
+});
